9964e7ae41a00c12d4e3e5416ca291dcd9ac727c
Edgar Mutugi <115105261+Edgar59@users.noreply.github.com>
2024-02-23 10:06:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleNav() {
+    var navLinks = document.getElementById("nav-links");
+    var navbarToggle = document.getElementsByClassName("navbar-toggle")[0];
+    
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+        navbarToggle.style.display = "none";
+    } else {
+        navLinks.style.display = "none";
+        navbarToggle.style.display = "flex";
+    }
+}
---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide all sections except the first one
+    var sections = document.querySelectorAll('.section');
+    for (var i = 1; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+
+    // Add event listeners to navigation links
+    var navLinks = document.querySelectorAll('.navigation a');
+    for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent default link behavior
+            var targetSectionId = this.getAttribute('href').substring(1);
+            showSection(targetSectionId);
+        });
+    }
+});
+
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('.section');
+    for (var i = 0; i < sections.length; i++) {
+        if (sections[i].id === sectionId) {
+            sections[i].style.display = 'block'; // Show the target section
+        } else {
+            sections[i].style.display = 'none'; // Hide other sections
+        }
+    }
+}
+    document.getElementById('logoutButton').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default anchor action
+        logout();
+    });
+
+    function logout() {
+        if (confirm("Are you sure you want to logout?")) {
+            alert("Logged out successfully!");
+            window.location.href = "index.html"; // Redirects to the home page or login page
+        }
+    }
+
+
+
